9a55892920fd637eed0f86ecaa86186f1c2af1be
Aoibhinn Nic Aoidh <oibind@pm.me>
2018-11-15 18:04:52 +0000

    Revert "tidy up code a little bit"
    This reverts commit 63a9035e120a70b27bb2d80175e78c53e733b2d0.

--- lambdas/getparrot.py
+++ lambdas/getparrot.py
@@ -4,13 +4,10 @@ import urllib.request
 
 
 def lambda_handler(event, context):
-    BUCKET_NAME = "parrot.today"
-    URL_LINKS = "parrots.txt"
-    IMAGE = "parrot.jpg"
-
     s3 = boto3.resource("s3")
-    s3.Bucket(BUCKET_NAME).download_file(URL_LINKS, URL_LINKS)
-    url = (random.choice(list(open(URL_LINKS))))
-    urllib.request.urlretrieve(url, IMAGE)
-    s3.Bucket(BUCKET_NAME).upload_file(IMAGE,
-                                       IMAGE, ExtraArgs={"ContentType": "image/jpeg", "ACL": "public-read"})
+    bucket = "parrot.today"
+    s3.Bucket(bucket).download_file("parrots.txt", "/tmp/parrots.txt")
+    url = (random.choice(list(open('/tmp/parrots.txt'))))
+    urllib.request.urlretrieve(url, "/tmp/parrot.jpg")
+    s3.Bucket(bucket).upload_file("/tmp/parrot.jpg",
+                                  "parrot.jpg", ExtraArgs={"ACL": "public-read"})

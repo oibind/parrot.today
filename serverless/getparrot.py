c131be7ea681d403f375632ea0a249e3bf8c4724
Aoibhinn Nic Aoidh <oibind@pm.me>
2019-02-08 15:12:19 +0000

    autopep8

--- serverless/getparrot.py
+++ serverless/getparrot.py
@@ -14,11 +14,15 @@ def getparrot(event, context):
     dirPath = "/tmp/"
     fileList = os.listdir(dirPath)
     for fileName in fileList:
-        os.remove(dirPath+"/"+fileName)
+        os.remove(dirPath + "/" + fileName)
 
     s3 = boto3.resource("s3")
-    s3.Bucket(BUCKET_NAME).download_file(URL_LIST, "/tmp/"+URL_LIST)
-    url = (random.choice(list(open("/tmp/"+URL_LIST))))
-    urllib.request.urlretrieve(url, "/tmp/"+IMG)
-    s3.Bucket(BUCKET_NAME).upload_file("/tmp/"+IMG, IMG,
-                                       ExtraArgs={"ContentType": "image/jpeg", "ACL": "public-read"})
+    s3.Bucket(BUCKET_NAME).download_file(URL_LIST, "/tmp/" + URL_LIST)
+    url = (random.choice(list(open("/tmp/" + URL_LIST))))
+    urllib.request.urlretrieve(url, "/tmp/" + IMG)
+    s3.Bucket(BUCKET_NAME).upload_file(
+        "/tmp/" + IMG,
+        IMG,
+        ExtraArgs={
+            "ContentType": "image/jpeg",
+            "ACL": "public-read"})
